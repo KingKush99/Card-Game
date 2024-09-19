5db277bcd9ee672243af1d1b7f7a262425847717
KingKush99 <connor.kudeba@gmail.com>
2024-09-19 14:22:24 -0400

    Added Card Class

--- Card Game.py
+++ Card Game.py
@@ -10,13 +10,18 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Card Game")
 
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
 
-Ace_Hearts = pygame.transform.scale(pygame.image.load('AceHearts.png').convert_alpha(), (50, 50))
-Ace_Clubs = pygame.transform.scale(pygame.image.load('AceClubs.png').convert_alpha(), (50, 50))
+class Card:
+  def __init__(self, suit, value, imageFileName):
+    self.suit = suit
+    self.value = value
+    self.image = pygame.transform.scale(pygame.image.load(imageFileName).convert_alpha(), (50, 50))
+
+
+
+
+Ace_Hearts = Card('H', 13, 'AceHearts.png')
+Ace_Clubs = Card('H', 13, 'AceClubs.png')
 
 
 
@@ -26,7 +31,7 @@ gameClock = pygame.time.Clock()
 
 # Main game loop
 while True:
-    screen.fill(WHITE)
+    screen.fill((255,255,255))
 
     # Handle window close
     for event in pygame.event.get():
@@ -39,8 +44,8 @@ while True:
     gameClock.tick(60)
 
     
-    screen.blit(Ace_Hearts, (250,500))
-    screen.blit(Ace_Clubs, (300,500))
+    screen.blit(Ace_Clubs.image, (300,500))
+    screen.blit(Ace_Hearts.image, (700,500))
 
 
 
